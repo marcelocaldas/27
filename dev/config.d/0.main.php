f1d0efaabfdd6bbc5798fb60108d35ad5a744ea4
Erlei Biazzio <erleibiazzio@gmail.com>
2024-04-09 10:58:28 -0300

    Habilita as GeoDivisions no ambiente de desenvolvimento

--- dev/config.d/0.main.php
+++ dev/config.d/0.main.php
@@ -1,4 +1,6 @@
 <?php
+use MapasCulturais\i;
+
 $_ENV['APP_MODE'] = 'development';
 $__process_assets = false;
 
@@ -28,4 +30,12 @@ return [
 
         'publishFolderCommand' => 'cp -R {IN} {PUBLISH_PATH}{FILENAME}'
     ]),
+    'app.geoDivisionsHierarchy' => [
+        'pais'              => ['name' => i::__('País'),            'showLayer' => true],
+        'estado'            => ['name' => i::__('Estado'),          'showLayer' => true],
+        'mesorregiao'       => ['name' => i::__('Mesorregião'),     'showLayer' => true],
+        'RI'  => ['name' => i::__('Região de integração'),'showLayer' => true],
+        'microrregiao'      => ['name' => i::__('Microrregião'),    'showLayer' => true],
+        'municipio'         => ['name' => i::__('Município'),       'showLayer' => true],
+    ]
 ];
