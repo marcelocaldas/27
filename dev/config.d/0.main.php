73e5427c478f14ac9a989867af796e639fedfc09
Welington De Lima Olegario <wolegario49@gmail.com>
2024-05-22 17:06:49 -0300

    Configura o Ambiente de desenvolvimento ajustando o nome do tema

--- dev/config.d/0.main.php
+++ dev/config.d/0.main.php
@@ -6,7 +6,7 @@ $__process_assets = false;
 
 return [
     /* MAIN */
-    'themes.active' => 'MapasCulturais\Themes\BaseV2',
+    'themes.active' => 'MapasPA',
     'app.mode' => $_ENV['APP_MODE'],
     'doctrine.isDev' => false, // deixe true somente se estiver trabalhando nos mapeamentos das entidades
     
