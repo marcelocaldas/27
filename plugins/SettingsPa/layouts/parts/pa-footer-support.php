dc462134ff409abbe3fced1dd8250850522d739a
marcelocaldas <38535161+marcelocaldas@users.noreply.github.com>
2025-02-18 09:07:36 -0300

    Update pa-footer-support.php

--- plugins/SettingsPa/layouts/parts/pa-footer-support.php
+++ plugins/SettingsPa/layouts/parts/pa-footer-support.php
@@ -1,4 +1,4 @@
 <div class="pa-footer-support">
-    Precisa de ajuda? <a href="https://bit.ly/3hOQfBz">Clique aqui</a> para falar com nossa equipe de suporte por chat. 
-    Ou envie um email para <a href="mailto:suportemapaculturalpa@gmail.com">suportemapaculturalpa@gmail.com</a>
+    Precisa de ajuda? <a href="https://tawk.to/chat/66bb4e430cca4f8a7a756299/1i55qjea9">Clique aqui</a> para falar com nossa equipe de suporte por chat. 
+    Ou envie um email para <a href="lpg.joaopessoa@gmail.com">lpg.joaopessoa@gmail.com</a>
 </div>
